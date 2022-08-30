b13af237c693b6824f63dc7103384fd8d86ce324
Ilya Sudakov <ilyasudakov@inbox.ru>
2022-08-30 19:53:51 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mx-auto py-2 sm:py-10">
+      <h1 className="text-3xl">Конвертация валюты</h1>
+    </div>
+  );
+}
+
+export default App;
