f056c92e69a5c62514389645140be02fb87981f9
Ilya Sudakov <ilyasudakov@inbox.ru>
2022-08-31 16:39:52 +0300

    Add tests for currencies functions

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 export default {
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$':
+      '<rootDir>/src/utils/tests/__mocks__/fileMock.js',
+    '\\.(css|sass|less|scss)$':
       '<rootDir>/src/utils/tests/__mocks__/fileMock.js',
   },
 };
--- src/pages/homepage/utils/currencies.test.ts
+++ src/pages/homepage/utils/currencies.test.ts
@@ -1,5 +1,16 @@
-import { formatNumberByCurrency, formatNumber } from './';
+import { formatNumberByCurrency, formatNumber, getCountryFromLocale } from './';
 
-test('Renders main page correctly', () => {
-  expect(true).toBeTruthy();
+describe('homepage/utils/currencies', () => {
+  test('formatNumber', () => {
+    expect(formatNumber(123)).toEqual('123');
+    expect(formatNumber(123.3129)).toEqual('123.313');
+  });
+  test('formatNumberByCurrency', () => {
+    expect(formatNumberByCurrency(123.2344, 'RUB')).toEqual(`RUB 123.23`);
+    expect(formatNumberByCurrency(123.2344, 'USD')).toEqual(`$123.23`);
+  });
+  test('getCountryFromLocale', () => {
+    expect(getCountryFromLocale('en', { EN: 'USD' })).toEqual('USD');
+    expect(getCountryFromLocale('en', { notexistent: 'USD' })).toEqual('RUB');
+  });
 });
